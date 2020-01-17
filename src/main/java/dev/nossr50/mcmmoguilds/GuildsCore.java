55dd2fdc45579209879b9a2d14e9687c0af8bf5b
nossr50 <nossr50@gmail.com>
2020-01-16 17:25:18 -0800

    Make it load

--- src/main/java/dev/nossr50/mcmmoguilds/GuildsCore.java
+++ src/main/java/dev/nossr50/mcmmoguilds/GuildsCore.java
@@ -1,13 +1,30 @@
 package dev.nossr50.mcmmoguilds;
 
+import com.gmail.nossr50.mcMMO;
+import dev.nossr50.mcmmoguilds.listeners.PlayerListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GuildsCore extends JavaPlugin {
 
+    private final mcMMO mcMMOPluginRef = null;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        if(Bukkit.getPluginManager().getPlugin("mcMMO").isEnabled()) {
+            //TODO: Add logic to make sure it's a compatible version of mcMMO
 
+            //Store reference
+            mcMMO mcMMOPlugin = (mcMMO) Bukkit.getPluginManager().getPlugin("mcMMO");
+
+            //Register listeners
+            Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+
+            Bukkit.getLogger().info("Guilds enabled.");
+        } else {
+            Bukkit.getLogger().info("Guilds failed to load.");
+        }
     }
 
     @Override
